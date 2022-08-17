05d0e502b49e3b984c9cae6ef2cc3414adb7886a
giridharguduru <giridharguduru@gmail.com>
2022-08-17 11:28:28 -0500

    Python Coding Challenge for dish

---
+++ Reverse.py
@@ -0,0 +1,10 @@
+# Python program to Reverse a number
+num = 2789
+reversed_num = 0
+
+while num != 0:
+    digit = num % 10
+    reversed_num = reversed_num * 10 + digit
+    num //= 10
+
+print("Reversed Number: " + str(reversed_num))
